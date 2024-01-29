a465f67093beeb17ea8e3a9c1dc83064c8a85cdf
mkrul <mishakrul@gmail.com>
2024-01-29 07:00:41 -0500

    add results json

--- export_data.py
+++ export_data.py
@@ -12,47 +12,49 @@ pt.tesseract_cmd = r'/usr/local/bin/tesseract'
 def find_breed_data():
   results = {}
   directories = os.listdir("images")
-  for dir in directories:
-    results[directories.index(dir)] = {
-      "images": [],
-      "breeds": {
-        "src": [],
-        "data": {}
-      }
-    }
-    print(f"Processing {dir}")
-    for filename in os.listdir(f"images/{dir}"):
-      image = Image.open(f"images/{dir}/{filename}")
-      text = pt.image_to_string(image)
-      if "%" in text:
-        print("Found breed data")
-        results[directories.index(dir)]["breeds"]["src"].append(f"images/{dir}/{filename}")
-      else:
-        print("Found dog image")
-        results[directories.index(dir)]["images"].append(f"images/{dir}/{filename}")
-
-    break
-  return results
-
-def process_breed_data():
-  breed_data = {}
-  results = find_breed_data()
-
-  display_breed_data(results)
-
+  num_dirs = len(directories)
+  iterator = 1
   with open("results.json", "w") as infile:
-    for each in results:
-      add_breed = 'y'
-      while add_breed == 'y':
-        breed = input("Breed name: ")
-        percentage = input("Percentage: ")
-        breed_data[breed] = percentage
-        results[each]["breeds"]["data"] = breed_data
-        add_breed = input("Add another breed? (y/n) ")
-
+    for dir in directories:
+      try:
+        results[directories.index(dir)] = {
+          "images": [],
+          "breeds": {
+            "src": [],
+            "data": {}
+          }
+        }
+        print(f"Processing {iterator}/{num_dirs}: {dir}")
+        iterator += 1
+        for filename in os.listdir(f"images/{dir}"):
+          image = Image.open(f"images/{dir}/{filename}")
+          text = pt.image_to_string(image)
+          if text != "":
+            print("Found breed data")
+            results[directories.index(dir)]["breeds"]["src"].append(f"images/{dir}/{filename}")
+          else:
+            print("Found dog image")
+            results[directories.index(dir)]["images"].append(f"images/{dir}/{filename}")
+      except:
+        print("Error")
+        continue
     json.dump(results, infile)
 
-def display_breed_data(results):
-  subprocess.run(['python3', 'display_breed_data.py', '--results', str(results)])
+# def process_breed_data():
+#   breed_data = {}
+#   results = find_breed_data()
 
-process_breed_data()
+#   with open("results.json", "w") as infile:
+#     for each in results:
+#       print(f"Processing {results[each]['breeds']['src']}")
+#       add_breed = 'y'
+#       while add_breed == 'y':
+#         breed = input("Breed name: ")
+#         percentage = input("Percentage: ")
+#         breed_data[breed] = percentage
+#         results[each]["breeds"]["data"] = breed_data
+#         add_breed = input("Add another breed? (y/n) ")
+
+#       json.dump(results, infile)
+
+find_breed_data()
