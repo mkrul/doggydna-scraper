d8cc418531fafcda14c9635fe6ab954f837f653d
mkrul <mishakrul@gmail.com>
2024-01-30 16:20:07 -0500

    add temp results

--- export_data.py
+++ export_data.py
@@ -5,6 +5,7 @@ from PIL import Image
 import cv2
 from pytesseract import pytesseract as pt
 import json
+import shutil
 import subprocess
 
 pt.tesseract_cmd = r'/usr/local/bin/tesseract'
@@ -16,12 +17,16 @@ def find_breed_data():
   iterator = 1
   with open("results.json", "w") as infile:
     for dir in directories:
+      # if no images exist
       try:
+        if len(os.listdir(f"images/{dir}")) <= 2:
+          print(f"Invalid number of images for {dir}")
+          shutil.rmtree(f"images/{dir}")
+          continue
         results[directories.index(dir)] = {
           "images": [],
-          "breeds": {
-            "src": [],
-            "data": {}
+          "data": {
+            "breeds": [],
           }
         }
         print(f"Processing {iterator}/{num_dirs}: {dir}")
@@ -29,32 +34,12 @@ def find_breed_data():
         for filename in os.listdir(f"images/{dir}"):
           image = Image.open(f"images/{dir}/{filename}")
           text = pt.image_to_string(image)
-          if text != "":
-            print("Found breed data")
-            results[directories.index(dir)]["breeds"]["src"].append(f"images/{dir}/{filename}")
-          else:
+          if text == "":
             print("Found dog image")
             results[directories.index(dir)]["images"].append(f"images/{dir}/{filename}")
       except:
         print("Error")
         continue
     json.dump(results, infile)
-
-# def process_breed_data():
-#   breed_data = {}
-#   results = find_breed_data()
-
-#   with open("results.json", "w") as infile:
-#     for each in results:
-#       print(f"Processing {results[each]['breeds']['src']}")
-#       add_breed = 'y'
-#       while add_breed == 'y':
-#         breed = input("Breed name: ")
-#         percentage = input("Percentage: ")
-#         breed_data[breed] = percentage
-#         results[each]["breeds"]["data"] = breed_data
-#         add_breed = input("Add another breed? (y/n) ")
-
-#       json.dump(results, infile)
 
 find_breed_data()
